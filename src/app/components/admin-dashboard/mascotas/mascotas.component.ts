9ccd4e7b7b494b527d1be5f7536e890b614041a4
jr256 <joser.72@outlook.com>
2023-12-02 17:41:52 -0500

    admin-mascota

--- src/app/components/admin-dashboard/mascotas/mascotas.component.ts
+++ src/app/components/admin-dashboard/mascotas/mascotas.component.ts
@@ -5,10 +5,12 @@ import { catchError, Observable, throwError } from "rxjs";
 import { Mascota } from 'src/app/models/mascota.model';
 import { MascotaService } from 'src/app/services/mascota.service';
 import { UsersService } from 'src/app/services/users.service';
-import { MatDialog } from '@angular/material/dialog';
+
 import { EmailExistsValidator } from 'src/app/validators/emailexists.validator';
 import { PageResponse } from 'src/app/models/page.response.model';
 import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mascotas',
@@ -16,74 +18,51 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./mascotas.component.css']
 })
 export class MascotasComponent implements OnInit{
+  formularioMascota!: FormGroup;
+  
 
-  searchFormGroup!: FormGroup;
-  mascotaFormGroup!: FormGroup;
-  pageMascotas!: Observable<PageResponse<Mascota>>;
-  errorMessage!: string;
-  currentPage: number = 0;
-  pageSize: number = 5;
-  submitted: boolean = false;
-
-  constructor(private dialog: MatDialog, private mascotaService: MascotaService, private fb: FormBuilder, private userService: UsersService) { }
+  constructor(private formBuilder: FormBuilder, 
+    private mascotaService: MascotaService, 
+    private fb: FormBuilder, 
+    private userService: UsersService,
+    private snackBar: MatSnackBar,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
-    this.searchFormGroup = this.fb.group({
-      keyword: this.fb.control('')
+    this.formularioMascota = this.formBuilder.group({
+      idmascota: [{value: '', disabled: true}],
+      nombremascota: ['', Validators.required],
+      nombrepropietario: ['', Validators.required],
+      user: this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      })
     });
-    this.mascotaFormGroup = this.fb.group({
-      nombremascota: ["", Validators.required],
-      nombrepropietario: ["", Validators.required],
-      user: this.fb.group({
-        email: ["", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")], [EmailExistsValidator.validate(this.userService)]],
-        password: ["", Validators.required],
-      })
-    })
-
-    this.handleSearchMascotas();
   }
 
-  handlePage(event: PageEvent) {
-    this.currentPage = event.pageIndex;
-    this.pageSize = event.pageSize;
-    this.handleSearchMascotas();
+  onSaveMascota(): void {
+    if (this.formularioMascota.valid) {
+      this.mascotaService.saveMascota(this.formularioMascota.value).subscribe({
+        next: response => {
+          this.formularioMascota.reset();
+
+          this.snackBar.open('Mascota registrada con éxito', 'Cerrar', {
+            duration: 3000,
+          });
+          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+            this.router.navigate(['/admin-dashboard/mascotas']);});
+          console.log(response);
+        },
+        error: error => {
+         
+          console.error('Hubo un error al guardar la mascota', error);
+        }
+      });
+    }
   }
 
-  getModal(content: any) {
-    const dialogRef = this.dialog.open(content, {width: '80%'});
-    this.submitted = false
-  }
 
-  handleSearchMascotas() {
-    let keyword = this.searchFormGroup.value.keyword;
-    this.pageMascotas = this.mascotaService.searchMascotas(keyword, this.currentPage, this.pageSize).pipe(
-      catchError(err => {
-        this.errorMessage = err.message;
-        return throwError(err);
-      })
-    )
-  }
-  
-  onSaveMascota() {
-    this.submitted = true;
-    if (this.mascotaFormGroup.invalid) return;
-    this.mascotaService.saveMascota(this.mascotaFormGroup.value).subscribe({
-      next: () => {
-        alert("Mascota guardada con éxito");
-        this.handleSearchMascotas();
-        this.mascotaFormGroup.reset();
-        this.submitted = false;
-        this.dialog.closeAll();
-      }, error: err => {
-        alert(err.message)
-      }
-    })
-  }
-
-  onCloseModal() {
-    this.dialog.closeAll();
-    this.mascotaFormGroup.reset();
-  }
 
 
 }
